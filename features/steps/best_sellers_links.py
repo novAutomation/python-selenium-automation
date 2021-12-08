00ddc37c40b9e9674a3c5e29c43607a8333fdbd2
Dom L <northshore@Doms-iMac.attlocal.net>
2021-12-07 21:04:06 -0600

    Homework 4 commit

---
+++ features/steps/best_sellers_links.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.by import By
+from behave import *
+
+
+BEST_SELLERS_LINKS = (By.CSS_SELECTOR, 'div#nav-xshop-container a')
+
+
+@given('User open Amazon Best Sellers Page')
+def user_open_amazon_best_seller_page(context):
+    context.driver.get('https://www.amazon.com/gp/bestsellers/?ref_=nav_cs_bestsellers')
+
+
+@then('User verify {expected_count} links')
+def user_verify_links_count(context, expected_count):
+    expected_count = int(expected_count)
+    links_count = context.driver.find_elements(*BEST_SELLERS_LINKS)
+    assert len(links_count) == expected_count, f'Error! Expected 30 Links but got {len(links_count)} Links'
---
+++ features/tests/best_sellers_links.feature
@@ -0,0 +1,6 @@
+# Created by Dom L. at 12/7/21
+Feature:Amazon Best Sellers Links
+
+  Scenario: User sees 30 best sellers links
+    Given User open Amazon Best Sellers Page
+    Then User verify 30 links
